41f6297927b0d1b78c444ac7ab6f414d365ac6da
Carlo Salcedo <csalcedo@bolste.com>
2016-09-16 11:12:23 -0700

    Change component to add position props

--- src/index.jsx
+++ src/index.jsx
@@ -383,25 +383,26 @@ export default React.createClass({
     let children;
 
     if (this.props.customInputComponent) {
-      <InputGroup ref="inputGroup" bsClass={this.props.bsClass} bsSize={this.props.bsSize} id={this.props.id ? this.props.id + "_group" : null}>
-        <Overlay rootClose={true} onHide={this.handleHide} show={this.state.focused} container={() => ReactDOM.findDOMNode(this.refs.overlayContainer)} target={() => ReactDOM.findDOMNode(this.refs.input)} placement={this.props.calendarPlacement} delayHide={200}>
-          <Popover id="calendar" title={calendarHeader} positionLeft="-44px" positionTop="28px">
-            <Calendar cellPadding={this.props.cellPadding} selectedDate={this.state.selectedDate} displayDate={this.state.displayDate} onChange={this.onChangeDate} dayLabels={this.props.dayLabels} />
-          </Popover>
-        </Overlay>
-        <div ref="overlayContainer" />
-        <input type="hidden" id={this.props.id} name={this.props.name} value={this.state.value || ''} />
-          {
-            children = React.Children.map(this.props.children, (child) => {
-              return React.cloneElement(child, {
-                onFocus: this.handleFocus,
-                onBlur: this.handleBlur,
-                focused: this.state.focused,
+      return (
+        <InputGroup ref="inputGroup" bsClass={this.props.bsClass} bsSize={this.props.bsSize} id={this.props.id ? this.props.id + "_group" : null}>
+          <Overlay rootClose={true} onHide={this.handleHide} show={this.state.focused} container={() => ReactDOM.findDOMNode(this.refs.overlayContainer)} target={() => ReactDOM.findDOMNode(this.refs.input)} placement={this.props.calendarPlacement} delayHide={200}>
+            <Popover id="calendar" title={calendarHeader} positionLeft="-44px" positionTop="28px">
+              <Calendar cellPadding={this.props.cellPadding} selectedDate={this.state.selectedDate} displayDate={this.state.displayDate} onChange={this.onChangeDate} dayLabels={this.props.dayLabels} />
+            </Popover>
+          </Overlay>
+          <div ref="overlayContainer" />
+          <input type="hidden" id={this.props.id} name={this.props.name} value={this.state.value || ''} />
+            {
+              React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, {
+                  onFocus: this.handleFocus,
+                  onBlur: this.handleBlur,
+                  focused: this.state.focused,
+                });
               });
-            });
-          }
-          {children}
-      </InputGroup>
+            }
+        </InputGroup>
+      );
     } else {
       return (
         <InputGroup ref="inputGroup" bsClass={this.props.bsClass} bsSize={this.props.bsSize} id={this.props.id ? this.props.id + "_group" : null}>
